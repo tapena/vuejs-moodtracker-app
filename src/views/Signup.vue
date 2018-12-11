ef56f4c001248707bb2620297b86a8d05f717fbd
drench <drench+github@gmail.com>
2018-12-10 20:14:27 -0600

    Update dash characters

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,23 +4,23 @@
       <form v-on:submit.prevent="submit()">
         <h1>Signup</h1>
         <ul>
-          <li class="text‐danger" v‐for="error in errors">{{ error }}</li>
+          <li class="text-danger" v-for="error in errors">{{ error }}</li>
         </ul>
-        <div class="form‐group">
+        <div class="form-group">
           <label>Name:</label>
-          <input type="text" class="form‐control" v‐model="name">
+          <input type="text" class="form‐control" v-model="name">
         </div>
-        <div class="form‐group">
+        <div class="form-group">
           <label>family_id:</label>
-          <input type="family_id" class="form‐control" v‐model="family_id">
+          <input type="family_id" class="form-control" v-model="family_id">
         </div>
-        <div class="form‐group">
+        <div class="form-group">
           <label>Password:</label>
-          <input type="password" class="form‐control" v‐model="password">
+          <input type="password" class="form-control" v-model="password">
         </div>
-        <div class="form‐group">
+        <div class="form-group">
           <label>Password confirmation:</label>
-          <input type="password" class="form‐control" v‐model="passwordConfirmation">
+          <input type="password" class="form‐control" v-model="passwordConfirmation">
         </div>
         <input type="submit" class="btn btn-warning" value="Submit">
       </form>
